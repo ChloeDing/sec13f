687d62bf2d751ede5db5459d5e83436edf5e0e68
Ding Liu <movingon.ding@gmail.com>
2018-02-18 16:34:30 -0700

    create a form to submit the old url and new url, when submit, compare the xmls pointed by the url. TODO: add method to handle the comparison and add ajax call to handle the div appending

--- server.js
+++ server.js
@@ -7,4 +7,15 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.post('/compare', (req, res) => {
+  const oldUrl = req.params.oldUrl;
+  const newUrl = req.params.newUrl;
+  var returnVal = compareXMLs(oldUrl, newUrl);
+  res.send(returnVal);
+})
+
+const compareXMLs = (oldUrl, newUrl) => {
+  return "something";
+};
+
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
