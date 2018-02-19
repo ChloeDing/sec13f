58d4086416b3b97da2f9798e001d4f040f33c19e
Ted Yang <yangxiongtan@gmail.com>
2018-02-19 23:12:43 +0000

    ajax completed, and use datatables

--- public/app.js
+++ public/app.js
@@ -1,16 +1,43 @@
 $(() => {
+
+  var createTrElement = (eachRow) => {
+    var $tr = $("<tr>");
+    $("<td>").text(eachRow['Company Name']).appendTo($tr);
+    $("<td>").text(eachRow['CUSIP']).appendTo($tr);
+    $("<td>").text(eachRow['Old Shares']).appendTo($tr);
+    $("<td>").text(eachRow['Old Value']).appendTo($tr);
+    $("<td>").text(eachRow['New Shares']).appendTo($tr);
+    $("<td>").text(eachRow['New Value']).appendTo($tr);
+    $("<td>").text((eachRow['Share Change'] * 100).toFixed(2) + '%').appendTo($tr);
+    $("<td>").text(eachRow['Note']).appendTo($tr);
+    return $tr;
+  };
+
   $('#compareButton').click((e) => {
     e.preventDefault();
     var data = {
-      oldUrl: $('#urlold').val(),
-      newUrl: $('#urlnew').val()
+      oldUrl: $('#oldurl').val(),
+      newUrl: $('#newurl').val()
     };
     $.ajax({
       type: "POST",
       url: '/compare',
       data: data
     }).done((data) => {
-      alert(data);
+      $('#resultTable').DataTable().destroy();
+      $('#resultTable tbody').empty();
+
+      data.forEach((eachRow) => {
+        var $tr = createTrElement(eachRow);
+        $tr.appendTo($('#resultTable tbody'));
+      });
+
+      $('#resultTable').DataTable({
+        "order": [[ 0, "asc" ]],
+        "lengthMenu": [ [-1, 10, 25, 50], ["All", 10, 25, 50] ]
+      });
+
+      $('#resultTable').removeClass("invisible");
     });
   });
 })
--- server.js
+++ server.js
@@ -28,8 +28,12 @@ app.post('/compare', (req, res) => {
           newXml += chunk;
         });
         response.on('end', () => {
-          const returnVal = compareXMLs(oldXml, newXml);
-          res.send(returnVal);
+          const oldVsNewList = compareXMLs(oldXml, newXml); //oldVsNewList is an object
+          var returnArray = [];
+          returnArray = Object.keys(oldVsNewList).map((eachCusip) => {
+            return oldVsNewList[eachCusip];
+          });
+          res.send(returnArray);
         });
       });
     });
@@ -37,13 +41,65 @@ app.post('/compare', (req, res) => {
 })
 
 const compareXMLs = (oldXml, newXml) => {
+  var oldVsNewList = {};
   parseString(oldXml, (err, result) => {
     recordArrayOld = result.informationTable.infoTable;
     parseString(newXml, (err, result) => {
       recordArrayNew = result.informationTable.infoTable;
-      return 'result';
+
+      oldList = {}; // Better formated
+      newList = {}; // Better formated
+      recordArrayOld.forEach((eachRecord) => {
+        var eachRow = {};
+        eachRow['Company Name'] = eachRecord.nameOfIssuer[0];
+        eachRow['Shares'] = eachRecord.shrsOrPrnAmt[0].sshPrnamt[0];
+        eachRow['Value'] = eachRecord.value[0];
+        oldList[eachRecord.cusip[0]] = eachRow;
+      });
+      recordArrayNew.forEach((eachRecord) => {
+        var eachRow = {};
+        eachRow['Company Name'] = eachRecord.nameOfIssuer[0];
+        eachRow['Shares'] = eachRecord.shrsOrPrnAmt[0].sshPrnamt[0];
+        eachRow['Value'] = eachRecord.value[0];
+        newList[eachRecord.cusip[0]] = eachRow;
+      });
+      for (var cusip in oldList) {
+        if (newList[cusip] === undefined) {
+          newList[cusip] = {};
+          newList[cusip]['Company Name'] = oldList[cusip]['Company Name'];
+          newList[cusip]['Shares'] = 0;
+          newList[cusip]['Value'] = 0;
+        }
+      }
+      for (var cusip in newList) {
+        if (oldList[cusip] === undefined) {
+          oldList[cusip] = {};
+          oldList[cusip]['Company Name'] = newList[cusip]['Company Name'];
+          oldList[cusip]['Shares'] = 0;
+          oldList[cusip]['Value'] = 0;
+        }
+      }
+      // Now oldList and newList have same list of companies
+      for (var cusip in oldList) {
+        oldVsNewList[cusip] = {};
+        var note = '';
+        if (oldList[cusip]['Company Name'] === newList[cusip]['Company Name']) {
+          oldVsNewList[cusip]['Company Name'] = oldList[cusip]['Company Name'];
+        } else {
+          note = note + 'Company name does not match.';
+          oldVsNewList[cusip]['Company Name'] = 'old: ' + oldList[cusip]['Company Name'] + ' vs new: ' + newList[cusip]['Company Name'];
+        }
+        oldVsNewList[cusip]['Old Shares'] = oldList[cusip]['Shares'];
+        oldVsNewList[cusip]['New Shares'] = newList[cusip]['Shares'];
+        oldVsNewList[cusip]['Old Value'] = oldList[cusip]['Value'];
+        oldVsNewList[cusip]['New Value'] = newList[cusip]['Value'];
+        oldVsNewList[cusip]['Share Change'] = ( newList[cusip]['Shares'] - oldList[cusip]['Shares'] ) / oldList[cusip]['Shares'];
+        oldVsNewList[cusip]['Note'] = note;
+        oldVsNewList[cusip]['CUSIP'] = cusip;
+      }
     });
   });
+  return oldVsNewList;
 };
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
