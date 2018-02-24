ebf31654d4879abc1eea1e5acad5d0061798aba5
Ted Yang <yangxiongtan@gmail.com>
2018-02-24 23:02:41 +0000

    added validations, delete, and external links

--- public/app.js
+++ public/app.js
@@ -15,6 +15,12 @@ $(() => {
 
   $('#compareButton').click((e) => {
     e.preventDefault();
+
+    if (!((validator.isURL($('#oldurl').val()) && $('#oldurl').val().slice(-4) === '.xml') && (validator.isURL($('#newurl').val()) && $('#newurl').val().slice(-4) === '.xml'))) {
+      alert('Please verify your inputs.');
+      return;
+    }
+    
     var data = {
       oldUrl: $('#oldurl').val(),
       newUrl: $('#newurl').val()
@@ -40,4 +46,35 @@ $(() => {
       $('#resultTable').removeClass("invisible");
     });
   });
+
+  $('#oldurl').change((e) => {
+    if (validator.isURL($('#oldurl').val()) && $('#oldurl').val().slice(-4) === '.xml') {
+      $("#okOld").addClass("text-success");
+    } else {
+      $("#okOld").removeClass("text-success");
+    }
+  });
+
+  $('#newurl').change((e) => {
+    if (validator.isURL($('#newurl').val()) && $('#newurl').val().slice(-4) === '.xml') {
+      $("#okNew").addClass("text-success");
+    } else {
+      $("#okNew").removeClass("text-success");
+    }
+  });
+
+  $('#clearOld').click((e) => {
+    $('#oldurl').val("").change();
+    $('#resultTable').addClass("invisible");
+    $('#resultTable').DataTable().destroy();
+    $('#resultTable tbody').empty();
+  });
+
+  $('#clearNew').click((e) => {
+    $('#newurl').val("").change();
+    $('#resultTable').addClass("invisible");
+    $('#resultTable').DataTable().destroy();
+    $('#resultTable tbody').empty();
+  });
+
 })
